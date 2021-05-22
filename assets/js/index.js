f39759361f064c5c21a1771033a03317ab960c47
ksrajath7 <rajathkizhiyedam@gmail.com>
2021-05-22 23:55:24 +0530

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,19 +21,32 @@ testimonials = [
         {"comment":"It has been a great year working with team Epoch Zero. Team Epoch Zero has been very supportive and flexible in meeting our demands.",
             "author":"Shabas Ahammed", "about":"Co-Founder, Estudo"},
         {"comment":"I am really impressed by the quality of services I received from Epoq Zero. You were right on schedule, charged reasonable prices, were professional and courteous in dealings, and delivered items well before time. I have got an efficient e-commerce platform for my services. My revenue has increased and I will definitely use your services again.",
-            "author":"Arun M", "about":"Founder, Robigo"},
-        {"comment":"I wanted to take a moment to thank you for the services your team has provided. Y our team has been a pleasure to work with, professional and timely. The only delay in work that we have experienced has been due to our own lack of organization managing our projects, not yours. Job well done and I hope we can continue to grow together.",
+            "author":"Arun M", "about":"Founder, The Traveller, UK"},
+        {"comment":"I wanted to take a moment to thank you for the services your team has provided. Your team has been a pleasure to work with, professional and timely. The only delay in work that we have experienced has been due to our own lack of organization managing our projects, not yours. Job well done and I hope we can continue to grow together.",
             "author":"Yoosof V", "about":"Director, Traverse 360 Consultation"},
         {"comment":"A robust congratulations to the team at Epoq Zero for a job well done. We've been trying to put together a functional website since we began our practice in April of 2018. I am happy to say we finally hired the e0 team and they've worked closely with us throughout the process, staying on task, on target, and on budget. I also appreciate their quick and courteous responses. I highly recommend their service!",
-            "author":"Evelyn Faye", "about":"Strategic Director, Ixora Club"},
-        // {"comment":"",
-        //     "author":"", "about":""},
-        // {"comment":"",
-        //     "author":"", "about":""},
-        // {"comment":"",
-        //     "author":"", "about":""},
+            "author":"Evelyn Faye", "about":"Strategic Director, Ixora Club, Jeddah, KSA"},
+        {"comment":"",
+            "author":"", "about":""},
+        {"comment":"",
+            "author":"", "about":""},
+        {"comment":"",
+            "author":"", "about":""},
     
     ]
+
+
+    function myFunction() {
+        setInterval(() => {
+            counter=(counter+1)%4
+            document.getElementById("comment").textContent=testimonials[counter].comment
+            document.getElementById("author").textContent=testimonials[counter].author
+            document.getElementById("about").textContent=testimonials[counter].about
+        }, 10000);
+      }
+
+      myFunction()
+   
 
 var counter = 0
 document.getElementById("comment").textContent=testimonials[counter].comment
@@ -57,3 +70,11 @@ function backComment(){
     document.getElementById("about").textContent=testimonials[counter].about
 
 }
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "300px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
